8a253319b38646852b55bd61ce677458da69cfe0
Ditnzii <adityaanugrah6767@gmail.com>
2026-08-23 21:16:27 +0700

    Refactor edukasi dan tambah halaman kosakata

---
+++ assets/js/kosakata.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $(".faq-header").on("click", function () {
+    const $content = $(this).next("#abjadContent");
+    const isOpening = !$content.is(":visible");
+
+    $content.stop(true, true).slideToggle(300, function () {
+      if (isOpening) {
+        $content.removeClass("hidden");
+      } else {
+        $content.addClass("hidden").css("display", "");
+      }
+    });
+
+    $(this).find("#faqArrow").toggleClass("rotate-90", isOpening);
+    $(this).attr("aria-expanded", isOpening);
+  });
+
+  $("#searchInput").on("input", function () {
+    const searchWords = $(this)
+      .val()
+      .toLowerCase()
+      .trim()
+      .split(/[\s,]+/)
+      .filter(Boolean);
+
+    $("#searchResults .search-item").each(function () {
+      const keywords = $(this).data("keywords").toLowerCase();
+      const isMatch =
+        searchWords.length === 0 ||
+        searchWords.some((word) => keywords.includes(word));
+      $(this).toggle(isMatch);
+    });
+  });
+});
