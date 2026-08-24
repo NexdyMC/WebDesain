a1f154d383a06c0239fa88d4f6d95c73f8bb0e0e
NexdyMC <febripratama1786@gmail.com>
2026-08-24 22:58:35 +0700

    update navbar button step 1

--- assets/js/edukasi.js
+++ assets/js/edukasi.js
@@ -142,3 +142,14 @@ $(document).ready(function () {
   });
 
 });
+
+
+$("#menuBtn").on("click", function () {
+  $("#mobileMenu, #iconOpen, #iconClose").toggleClass("hidden grid");
+});
+
+$("#mobileMenu a").on("click", function () {
+  $("#mobileMenu").addClass("hidden").removeClass("grid");
+  $("#iconOpen").addClass("grid").removeClass("hidden");
+  $("#iconClose").addClass("hidden").removeClass("grid");
+});
--- assets/js/kosakata.js
+++ assets/js/kosakata.js
@@ -32,3 +32,14 @@ $(document).ready(function () {
     });
   });
 });
+
+
+$("#menuBtn").on("click", function () {
+  $("#mobileMenu, #iconOpen, #iconClose").toggleClass("hidden grid");
+});
+
+$("#mobileMenu a").on("click", function () {
+  $("#mobileMenu").addClass("hidden").removeClass("grid");
+  $("#iconOpen").addClass("grid").removeClass("hidden");
+  $("#iconClose").addClass("hidden").removeClass("grid");
+});
